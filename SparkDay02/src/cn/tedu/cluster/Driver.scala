d4ed1e48b420a0dc3fd3fe9898af5a03874034b5
linziming <871440130@qq.com>
2021-02-01 02:32:54 +0800

    spark-day02 集群 jar包

--- SparkDay02/src/cn/tedu/cluster/Driver.scala
+++ SparkDay02/src/cn/tedu/cluster/Driver.scala
@@ -5,10 +5,15 @@ import org.apache.spark.SparkContext
 
 object Driver {
   def main(args: Array[String]): Unit = {
-    val conf=new SparkConf().setMaster("spark://hadoop01:7077").setAppName("")
+    // setMaster("spark://hadoop01:7077"):设置为集群的运行模式
+    val conf=new SparkConf().setMaster("spark://hadoop01:7077").setAppName("wordcount")
     val sc=new SparkContext(conf)
     
     val data=sc.textFile("hdfs://hadoop01:9000/01.txt", 3)
+    
+    val wordcount=data.flatMap { _.split(" ") }.map{(_,1)}.reduceByKey{_+_}
+    
+    wordcount.saveAsTextFile("hdfs://hadoop01:9000/2008-result01")
     
   }
 }
